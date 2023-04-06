fa07a9a84c79e142191ff1fb9936c841b1d36033
jens-sam <114012084+jens-sam@users.noreply.github.com>
2023-04-06 09:05:54 -0600

    Create hw11_lib.h

---
+++ Homework 11/hw11_lib.h
@@ -0,0 +1,24 @@
+
+
+// hw11_lib.h
+#ifndef HW11_LIB_H
+#define HW11_LIB_H
+
+typedef struct Student {
+    char name[50];
+    float grade;
+    struct Student *next;
+} Student;
+
+void studentAdd(char *name, float grade);
+void studentRemove(char *name);
+Student *studentFind(char *name);
+void studentPrint(Student *student);
+void studentPrintList();
+float studentGetGrade(char *name);
+void studentLoad(char *filename);
+int studentCount();
+void studentDeleteList();
+float studentGetAverage();
+
+#endif // HW11_LIB_H
