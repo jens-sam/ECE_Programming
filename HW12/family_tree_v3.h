00f389b38e2c2d6d0a7753e68d3eabb69d5620f2
jens-sam <114012084+jens-sam@users.noreply.github.com>
2023-04-14 16:35:38 -0600

    Restarted - family tree current

---
+++ HW12/family_tree_v3.h
@@ -0,0 +1,26 @@
+#ifndef FAMILY_TREE_H
+#define FAMILY_TREE_H
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+struct Person {
+    char father[50];
+    char mother[50];
+    char name[20];
+    struct Person *next;
+    struct Person *prev;
+};
+
+struct Person *findHead(struct Person *n);
+struct Person *findTail(struct Person *n);
+struct Person *FindPerson(struct Person *n, const char *name);
+struct Person *addToTail(struct Person *n, const char *name, const char *father, const char *mother);
+const char *findFather(struct Person *n, const char *name);
+const char *findMother(struct Person *n, const char *name);
+void findAncestor(struct Person *n, const char *name, int gen);
+void findDescendants(struct Person *n, const char *name, int depth);
+void printAll(struct Person *n);
+
+#endif //FAMILY_TREE_H
